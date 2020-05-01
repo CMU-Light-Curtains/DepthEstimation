196de43f99873be3bebb52c338ca13ffd4d9d801
raaj <yaadhavraaj@gmail.com>
2020-05-01 12:45:46 -0400

    updated

---
+++ external/compile.sh
@@ -0,0 +1,27 @@
+sudo apt-get install libpng++-dev
+cd deval_lib
+rm -rf build; mkdir build; cd build;
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python ..
+make -j4
+cp pyevaluatedepth_lib.so ../
+cd ..; cd ..
+cd perception_lib
+rm -rf build; mkdir build; cd build;
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python ..
+make -j4
+cp pyperception_lib.so ../
+cp libperception_lib.so ../
+cd ..; cd ..
+cd utils_lib
+rm -rf build; mkdir build; cd build;
+make -j4
+cp utils_lib.so ../
+cd ..; cd ..
+cd lcsim
+git submodule init
+git submodule update
+rm -rf build; mkdir build; cd build;
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so ..
+make -j4
+cd ..; cd ..;
+
---
+++ external/test.py
@@ -0,0 +1,4 @@
+import deval_lib.pyevaluatedepth_lib
+import perception_lib.viewer
+import utils_lib.utils_lib
+import lcsim.python.sim
